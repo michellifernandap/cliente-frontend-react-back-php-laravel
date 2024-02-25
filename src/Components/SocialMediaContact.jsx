a16c11ccde3c2ee527f838a3c4ca63df0302755d
Michelli Prieto <Michellifernanda.fp@gmail.com>
2024-02-25 11:54:16 +0100

    ok

---
+++ src/Components/SocialMediaContact.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "../Styles/components/SocialMediaContact.css"
+
+
+class SocialMediaContact extends React.Component {
+  render() {
+    return (
+      <section className="Social-media py-2 mx-4 ">
+       <footer class="w-100  d-flex  align-items-center justify-content-center flex-wrap">
+    <p class="fs-5 px-3 pt-3">• ♡ STATIONARY WITH LOVE ♡ •</p>
+    <p class="fs-5 px-3 pt-3"> &copy; Todos Los Derechos Reservados 2021</p>
+  </footer>
+      </section>
+    );
+  }
+}
+export default SocialMediaContact;
